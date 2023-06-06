8217d1664187cbff1032b29912e9276351e6ab86
IrsyadRafiNaufaldi <125427599+IrsyadRafiNaufaldi@users.noreply.github.com>
2023-06-06 22:43:12 +0700

    tamba file

---
+++ Final Project/view more product/mod.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ATOMIZER</title>
+
+    <link rel="stylesheet" href="viewmoreproduct.css">
+</head>
+<body>
+    <div class="bgheader">
+        <header>
+            <p>Welcome to Vape. salah satu online vape shop terbaik di Indonesia</p>
+        </header>
+    </div>
+
+    <div class="bars">
+        <div class="gambars">
+            <a href="../home/HOME.php"><img src="image/logovapehitam.png" alt=""></a>
+        </div>
+        <div class="button-choice">
+                <a href="all.php">All</a>  
+                <a href="atomizer.php">Atomizer</a> 
+                <a class="active" href="">Mod</a> 
+                <a href="liquid.php">Liquid</a> 
+                <a href="baterai.php">Baterai</a>
+        </div>
+    </div>
+    
+    <div class="bgfooter">
+        <div class="box3">
+            <img src="image/logo.png" alt="">
+        </div>
+        <div class="box4">
+            <p>Follow us on</p>
+            <div class="sosmed">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-whatsapp"></i>
+            </div>
+        </div>
+    </div>
+
+    <footer>Copyright 2023 © VAP.COM</footer>
+
+    <script src="https://kit.fontawesome.com/73bcd336f4.js"></script>
+</body>
+</html>
