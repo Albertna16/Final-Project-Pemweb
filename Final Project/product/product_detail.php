deb34e2cf9096934a11c474f1c2ad82a21e8151f
Mohammad Habim Hazidan Rifqi <125388260+rfqhabim@users.noreply.github.com>
2023-06-07 00:58:08 +0700

    Add files via upload

---
+++ Final Project/product/product_detail.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PRODUCT DETAIL</title>
+
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    />
+    
+    <!--import gfonts-->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400&display=swap');
+    </style>
+
+    <!--link font awesome-->
+    <script src="https://kit.fontawesome.com/ad6991be8a.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+  
+  </head>
+  <body>
+    <div class="bgheader" id="home">
+      <header>
+        <p>Welcome to Vape. salah satu online vape shop terbaik di Indonesia</p>
+      </header>
+    </div>
+
+    <div class="bar">
+      <div class="gambar">
+          <a href="#"><img src="image/logovapehitam.png" alt=""></a>
+      </div>
+      <div class="tombol">
+          <a href="#home">HOME</a>
+          <a href="#product">PRODUCT</a>
+      </div>
+    </div>
+
+    <div classc="container-main">
+        <div class="product-container">
+            <div class="product-box1">
+                <img src="image/fotoProduk.png" alt="Gambar Produk" />
+                <button><a href=""> Beli Sekarang </a></button>
+            </div>
+            <div class="product-box2">
+                <h3>RDA Druga 24mm</h3>
+                <h6>Rp 115.000</h6>
+                <p>
+                RDA Druga 24mm adalah sebuah produk vape inovatif yang menghadirkan
+                pengalaman vaping yang luar biasa bagi pengguna. RDA (Rebuildable
+                Dripping Atomizer) ini menonjolkan kualitas dan desain yang sempurna,
+                dengan fokus pada produksi rasa yang kaya dan penghasilan asap yang
+                melimpah.
+                <br><br>
+                Dengan diameter 24mm, RDA Druga menawarkan ukuran yang ideal untuk
+                kompatibilitas dengan sebagian besar mod vape yang tersedia di
+                pasaran. Terbuat dari bahan berkualitas tinggi seperti stainless
+                steel, RDA ini menawarkan daya tahan yang kuat dan tahan lama, serta
+                tampilan yang menarik.
+                </p>
+            </div>
+        </div>
+    </div>  
+
+    <div class="bgfooter">
+        <div class="box3">
+            <img src="image/logovape.png" alt="">
+        </div>
+        <div class="box4">
+            <p>Follow us on</p>
+            <div class="sosmed">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <footer><p>Copyright 2023 © VAP.COM</p></footer>
+  </body>
+</html>
