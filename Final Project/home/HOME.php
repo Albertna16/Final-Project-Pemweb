145f673cbca9dda35b708dc24a48e09e2cb0ff56
IrsyadRafiNaufaldi <125427599+IrsyadRafiNaufaldi@users.noreply.github.com>
2023-06-06 16:59:06 +0700

    home php

---
+++ Final Project/home/HOME.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+
+    <link rel="stylesheet" href="home.css">
+
+</head>
+<body>
+    <div class="bgheader" id="home">
+        <header>
+            <p>Welcome to Vape. salah satu online vape shop terbaik di Indonesia</p>
+        </header>
+    </div>
+
+    <div class="bar">
+        <div class="gambar">
+            <img src="image/logo.png" alt="">
+        </div>
+        <div class="tombol">
+            <a href="#home">HOME</a>
+            <a href="#aboutus">ABOUT US</a>
+            <a href="#product">PRODUCT</a>
+            <a href="#report">REPORT</a>
+            <div class="profile">
+                <i class="fa-solid fa-user"><a href=""></a></i>
+            </div>
+            <div class="logout">
+                <i class="fa-solid fa-right-from-bracket"><a href=""></a></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="jumbotron">
+        <img src="image/home.png" alt="">
+    </div>
+
+    <div class="aboutus" id="aboutus">
+        <div class="box1">
+            <img src="image/pod.png" alt="">
+        </div>
+        <div class="box2">
+            <h2>About Us</h2>
+          <p>Selamat datang di VapeStore.com, destinasi terpercaya Anda untuk semua kebutuhan vaping! 
+            Kami adalah toko online yang berkomitmen untuk menyediakan produk-produk vape berkualitas 
+            tinggi, aksesori terbaru, serta informasi dan layanan pelanggan yang luar biasa. <br><br> 
+            VapeStore.com, kami memahami bahwa vaping telah menjadi fenomena global yang meraih popularitas 
+            yang luar biasa. Kami berdedikasi untuk menyediakan platform yang lengkap dan mudah digunakan 
+            bagi para penggemar vaping dari berbagai kalangan, baik bagi pemula yang tertarik untuk memulai 
+            vaping maupun bagi vapers berpengalaman yang mencari produk terbaru dan terbaik.</p>
+        </div>
+    </div>
+
+    <div class="product" id="product">
+        <div class="kalimat_kategori">
+            <h2>Product Categories</h2>
+            <p>Kami bangga menyajikan beragam produk vape berkualitas tinggi di VapeStore.com. Dari rokok 
+            elektronik inovatif hingga e-liquid dengan rasa terbaik, kami memiliki semua yang Anda butuhkan 
+            untuk mengalami vaping yang memuaskan.
+            </p>
+        </div>
+        <div class="kategori">
+            <a href="view more product/atomizer.html"><img src="image/product1.png" alt=""></a>
+            <a href=""><img src="image/product2.png" alt=""></a>
+            <a href=""><img src="image/product3.png" alt=""></a>
+            <a href=""><img src="image/product4.png" alt=""></a>
+        </div>
+        <div class="view-more">
+            <a href="">View More</a>
+        </div>
+    </div>
+
+    <div class="report" id="report">
+        <div class="box2">
+            <div class="box2-1">
+                <h2>Report</h1>
+                <p>Kami menghargai setiap saran dan kritik yang Anda miliki untuk meningkatkan pengalaman Anda 
+                di Vape Store. Jika Anda ingin melaporkan, jangan ragu untuk. Tim kami siap menjadi 
+                penghubung dan merespons setiap umpan balik yang Anda berikan.
+                </p>
+            </div>
+            <div class="box2-2">
+                <div class="nama">
+                    <div class="nama1">
+                        <i class="fa-regular fa-user"></i>
+                        <input type="text" name="FN" placeholder="First Name">
+                    </div>
+                    <div class="nama1">
+                        <i class="fa-regular fa-user"></i>
+                        <input type="text" name="LN" placeholder="Last Name">
+                    </div>
+                </div>
+
+                <div class="email">
+                    <i class="fa-regular fa-envelope"></i>
+                    <input type="text" name="email" placeholder="email">
+                </div>
+
+                <div class="desc">
+                    <textarea type="text" name="issue" placeholder="Describe your issue"></textarea>
+                </div>
+
+                <div class="send">
+                    <a href="#">Send</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bgfooter">
+        <div class="box3">
+            <img src="image/logo1.png" alt="">
+        </div>
+        <div class="box4">
+            <p>Follow us on</p>
+            <div class="sosmed">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-whatsapp"></i>
+            </div>
+        </div>
+    </div>
+
+    <footer>Copyright 2023 © VAP.COM</footer>
+
+    <script src="https://kit.fontawesome.com/73bcd336f4.js"></script>
+    
+</body>
+</html>
