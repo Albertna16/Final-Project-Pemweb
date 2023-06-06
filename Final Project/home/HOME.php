8585e90ac01a7e953892895f2df6a06d5e911944
Albertna16 <muhammadalbert16@gmail.com>
2023-06-06 22:12:59 +0700

    modified home and connect home to database

--- Final Project/home/HOME.php
+++ Final Project/home/HOME.php
@@ -1,5 +1,55 @@
+<?php
+
+// membuat koneksi ke system
+$dbServer = 'localhost';
+$dbUser = 'root';
+$dbPass = '';
+$dbName = "pemwebvape";
+
+try {
+    //membuat object PDO untuk koneksi ke database
+    $connection = new PDO("mysql:host=$dbServer;dbname=$dbName", $dbUser, $dbPass);
+    // setting ERROR mode PDO: ada tiga mode error mode silent, warning, exception
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $err) {
+    echo "Failed Connect to Database Server : " . $err->getMessage();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nama = $_POST['FN'];
+    $email = $_POST['email'];
+    $deskripsi = $_POST['issue'];
+
+    // Mencari ID_USER berdasarkan email yang diinputkan
+    $query = "SELECT ID_USER FROM user WHERE EMAIL_USER = :email";
+    $statement = $connection->prepare($query);
+    $statement->bindValue(':email', $email);
+    $statement->execute();
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if ($result && isset($result['ID_USER'])) {
+        $id_user = $result['ID_USER'];
+
+        // Menyimpan data ke tabel report
+        $query = "INSERT INTO report (ID_USER, DESK_REPORT) VALUES (:id_user, :deskripsi)";
+        $statement = $connection->prepare($query);
+        $statement->bindValue(':id_user', $id_user);
+        $statement->bindValue(':deskripsi', $deskripsi);
+        $statement->execute();
+
+        // Menampilkan pesan setelah data berhasil disimpan
+        echo "Report submitted successfully!";
+    } else {
+        echo "User not found!";
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,7 +61,7 @@
 
     <!--import gfonts-->
     <style>
-      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400&display=swap');
     </style>
 
     <!--link font awesome-->
@@ -19,6 +69,7 @@
 
 
 </head>
+
 <body>
     <div class="bgheader" id="home">
         <header>
@@ -54,22 +105,22 @@
         </div>
         <div class="box2">
             <h2>About Us</h2>
-          <p>Selamat datang di VapeStore.com, destinasi terpercaya Anda untuk semua kebutuhan vaping! 
-            Kami adalah toko online yang berkomitmen untuk menyediakan produk-produk vape berkualitas 
-            tinggi, aksesori terbaru, serta informasi dan layanan pelanggan yang luar biasa. <br><br> 
-            VapeStore.com, kami memahami bahwa vaping telah menjadi fenomena global yang meraih popularitas 
-            yang luar biasa. Kami berdedikasi untuk menyediakan platform yang lengkap dan mudah digunakan 
-            bagi para penggemar vaping dari berbagai kalangan, baik bagi pemula yang tertarik untuk memulai 
-            vaping maupun bagi vapers berpengalaman yang mencari produk terbaru dan terbaik.</p>
+            <p>Selamat datang di VapeStore.com, destinasi terpercaya Anda untuk semua kebutuhan vaping!
+                Kami adalah toko online yang berkomitmen untuk menyediakan produk-produk vape berkualitas
+                tinggi, aksesori terbaru, serta informasi dan layanan pelanggan yang luar biasa. <br><br>
+                VapeStore.com, kami memahami bahwa vaping telah menjadi fenomena global yang meraih popularitas
+                yang luar biasa. Kami berdedikasi untuk menyediakan platform yang lengkap dan mudah digunakan
+                bagi para penggemar vaping dari berbagai kalangan, baik bagi pemula yang tertarik untuk memulai
+                vaping maupun bagi vapers berpengalaman yang mencari produk terbaru dan terbaik.</p>
         </div>
     </div>
 
     <div class="product" id="product">
         <div class="kalimat_kategori">
             <h2>Product Categories</h2>
-            <p>Kami bangga menyajikan beragam produk vape berkualitas tinggi di VapeStore.com. Dari rokok 
-            elektronik inovatif hingga e-liquid dengan rasa terbaik, kami memiliki semua yang Anda butuhkan 
-            untuk mengalami vaping yang memuaskan.
+            <p>Kami bangga menyajikan beragam produk vape berkualitas tinggi di VapeStore.com. Dari rokok
+                elektronik inovatif hingga e-liquid dengan rasa terbaik, kami memiliki semua yang Anda butuhkan
+                untuk mengalami vaping yang memuaskan.
             </p>
         </div>
         <div class="kategori">
@@ -87,35 +138,30 @@
         <div class="box2">
             <div class="box2-1">
                 <h2>Report</h1>
-                <p>Kami menghargai setiap saran dan kritik yang Anda miliki untuk meningkatkan pengalaman Anda 
-                di Vape Store. Jika Anda ingin melaporkan, jangan ragu untuk. Tim kami siap menjadi 
-                penghubung dan merespons setiap umpan balik yang Anda berikan.
-                </p>
+                    <p>Kami menghargai setiap saran dan kritik yang Anda miliki untuk meningkatkan pengalaman Anda
+                        di Vape Store. Jika Anda ingin melaporkan, jangan ragu untuk. Tim kami siap menjadi
+                        penghubung dan merespons setiap umpan balik yang Anda berikan.
+                    </p>
             </div>
             <div class="box2-2">
-                <div class="nama">
-                    <div class="nama1">
-                        <i class="fa-regular fa-user"></i>
-                        <input type="text" name="FN" placeholder="First Name">
+                <form action="" method="POST">
+                    <div class="nama">
+                        <div class="nama1">
+                            <i class="fa-regular fa-user"></i>
+                            <input type="text" name="FN" placeholder="Name">
+                        </div>
                     </div>
-                    <div class="nama1">
-                        <i class="fa-regular fa-user"></i>
-                        <input type="text" name="LN" placeholder="Last Name">
+                    <div class="email">
+                        <i class="fa-regular fa-envelope"></i>
+                        <input type="text" name="email" placeholder="email">
                     </div>
-                </div>
-
-                <div class="email">
-                    <i class="fa-regular fa-envelope"></i>
-                    <input type="text" name="email" placeholder="email">
-                </div>
-
-                <div class="desc">
-                    <textarea type="text" name="issue" placeholder="Describe your issue"></textarea>
-                </div>
-
-                <div class="send">
-                    <a href="#">Send</a>
-                </div>
+                    <div class="desc">
+                        <textarea type="text" name="issue" placeholder="Describe your issue"></textarea>
+                    </div>
+                    <div class="send">
+                        <button type="submit">Send</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -137,6 +183,7 @@
     <footer>Copyright 2023 © VAP.COM</footer>
 
     <script src="https://kit.fontawesome.com/73bcd336f4.js"></script>
-    
+
 </body>
+
 </html>
