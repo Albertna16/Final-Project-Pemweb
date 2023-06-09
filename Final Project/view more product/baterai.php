09756f71554354a507f277c29faa2c8b787c1c1e
Mohammad Habim Hazidan Rifqi <125388260+rfqhabim@users.noreply.github.com>
2023-06-09 13:24:23 +0700

    Update baterai.php

--- Final Project/view more product/baterai.php
+++ Final Project/view more product/baterai.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ATOMIZER</title>
+    <title>BATERAI</title>
 
-    <link rel="stylesheet" href="viewmoreproduct.css">
+    <link rel="stylesheet" href="styleviewmore.css">
+    <link rel="stylesheet" href="reset.css">
+
+    <!--link font awesome-->
+    <script src="https://kit.fontawesome.com/ad6991be8a.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="bgheader">
@@ -15,19 +19,148 @@
         </header>
     </div>
 
-    <div class="bars">
-        <div class="gambars">
-            <a href="../home/HOME.php"><img src="image/logovapehitam.png" alt=""></a>
+    <div class="bar">
+        <div class="gambar">
+            <a href="../home/home.php"><img src="image/logovapehitam.png" alt=""></a>
         </div>
         <div class="button-choice">
-                <a href="all.php">All</a>  
-                <a href="atomizer.php">Atomizer</a> 
-                <a href="mod.php">Mod</a> 
-                <a href="liquid.php">Liquid</a> 
-                <a class="active" href="">Baterai</a>
+            <a href="all.php">All</a>
+            <a href="atomizer.php">Atomizer</a>
+            <a href="mod.php">Mod</a>
+            <a href="liquid.php">Liquid</a>
+            <a class="nav-link active" href="baterai.php">Baterai</a>
         </div>
     </div>
-    
+
+    <div class="container-main">
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card" style="background-color: #b5b0b0;">
+                <div class="image">
+                    <img src="image/card1.png" class="card-img-top" alt="...">
+                </div>
+                <div class="text">
+                    <h4>RDA Druga 24mm</h4>
+                    <p>Rp.115.000</p>
+                </div>
+                <button>
+                    <a href="#">beli</a>
+                </button>
+            </div>
+        </div>
+    </div>
+
     <div class="bgfooter">
         <div class="box3">
             <img src="image/logo.png" alt="">
@@ -35,15 +168,13 @@
         <div class="box4">
             <p>Follow us on</p>
             <div class="sosmed">
-                <i class="fa-brands fa-facebook"></i>
-                <i class="fa-brands fa-instagram"></i>
-                <i class="fa-brands fa-whatsapp"></i>
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
             </div>
         </div>
     </div>
 
-    <footer>Copyright 2023 © VAP.COM</footer>
-
-    <script src="https://kit.fontawesome.com/73bcd336f4.js"></script>
+    <footer><p>Copyright 2023 © VAP.COM</p></footer>
 </body>
 </html>
